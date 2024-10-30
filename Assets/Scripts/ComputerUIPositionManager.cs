7a883404a6d1c8550ffbed3526798ec4defe1782
kasjaduddin <kasjaduddin@outlook.com>
2024-10-30 20:34:38 +0700

    chore: update documentation comment for computer UI position manager script

--- Assets/Scripts/ComputerUIPositionManager.cs
+++ Assets/Scripts/ComputerUIPositionManager.cs
@@ -6,27 +6,33 @@ public class ComputerUIPositionManager : MonoBehaviour
     private GameObject screen; // Computer screen game object
     [SerializeField]
     private GameObject keyboard; // Computer keyboard game object
+
     // Start is called before the first frame update
     void Start()
     {
         // Set main camera
         mainCamera = Camera.main;
 
+        // Set initial screen static position and rotation
         if (screen != null)
         {
-            // Set initial static position and rotation
             screen.transform.position = new Vector3(screen.transform.position.x, screen.transform.position.y, screen.transform.position.z);
             screen.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
 
+        // Set initial keyboard static position and rotation
         if (keyboard != null)
             keyboard.transform.position = new Vector3(keyboard.transform.position.x, keyboard.transform.position.y, keyboard.transform.position.z);        
     }
+
+    // OnEnable is called when the object becomes enabled and active
+
     private void OnEnable()
     {
         Invoke("SetScreenPosition", 0.1f);
         Invoke("SetKeyboardPosition", 0.1f);
     }
+
     // Set screen position
     public void SetScreenPosition()
     {
@@ -34,6 +40,8 @@ public class ComputerUIPositionManager : MonoBehaviour
         Vector3 screenPosition = new Vector3(screen.transform.position.x, cameraPosition.y, cameraPosition.z + 0.4f);
         screen.transform.position = screenPosition;
     }
+
+    // Set keyboard position
     public void SetKeyboardPosition()
     {
         Vector3 cameraPosition = mainCamera.transform.position;
