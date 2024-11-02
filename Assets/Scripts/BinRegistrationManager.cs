6d8b0b9c63b3053996fc14007c5bb0b90afcec24
kasjaduddin <kasjaduddin@outlook.com>
2024-11-03 06:39:51 +0700

    chore: update documentation comment for bin registration manager script

--- Assets/Scripts/BinRegistrationManager.cs
+++ Assets/Scripts/BinRegistrationManager.cs
@@ -3,25 +3,19 @@ using TMPro;
 
 public class BinRegistrationManager : MonoBehaviour
 {
-    public GoogleFormConnector connector;
-    public TextMeshProUGUI codeInputField;
-    public TextMeshProUGUI informationInputField;
-    private string code;
-    private string information;
-    void Start()
-    {
+    public GoogleFormConnector connector; // Reference to the Google Form connector
+    public TextMeshProUGUI codeInputField; // Input field for the bin code
+    public TextMeshProUGUI informationInputField; // Input field for additional information
+    private string code; // Variable to store the bin code
+    private string information; // Variable to store the additional information
 
-    }
+    // Register new bin to system
     public void AddNewBin()
     {
         code = codeInputField.text;
         information = informationInputField.text;
 
+        // Write data to the Google Form
         StartCoroutine(connector.WriteData(code, information));
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
