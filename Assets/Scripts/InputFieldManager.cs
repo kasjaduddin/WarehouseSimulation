53bf1f6549019e37d7e90d4ddc9c900c1cdab577
kasjaduddin <kasjaduddin@outlook.com>
2024-11-19 10:11:58 +0700

    feat: update input field manager

--- Assets/Scripts/InputFieldManager.cs
+++ Assets/Scripts/InputFieldManager.cs
@@ -9,37 +9,16 @@ public class InputFieldManager : MonoBehaviour
     public TMP_InputField informationInputField; // Input field for bin information
     public TMP_InputField usernameInputField; // Input field for user name
     public TMP_InputField passwordInputField; // Input field for account password
+    public TMP_InputField binCodeInputField; // Input field for bin code
+    public TMP_InputField tagDescriptionInputField; // Input field for tag description
     public TMP_InputField numberOfPeriodsField; // Input field for number of periods
 
-    // Set the output field of the keyboard manager to the bin code input field
-    public void InputCode()
-    {
-        keyboardManager.outputField = codeInputField;
-    }
-
-    // Set the output field of the keyboard manager to the bin information input field
-    public void InputInformation()
-    {
-        keyboardManager.outputField = informationInputField;
-    }
-
-    // Set the output field of the keyboard manager to the user name input field
-    public void InputUsername()
-    {
-        keyboardManager.outputField = usernameInputField;
-    }
-
-    // Set the output field of the keyboard manager to the account password input field
-    public void InputPassword()
-    {
-        keyboardManager.outputField = passwordInputField;
-    }
     public void InputNumberOfPeriods()
     {
         keyboardManager.outputField = numberOfPeriodsField;
     }
-
-    public void InputQuantityRecord(TMP_InputField inputField) 
+    // Set the output field of the keyboard manager to the input field
+    public void KeyboardInput(TMP_InputField inputField) 
     { 
         keyboardManager.outputField = inputField;
     }
