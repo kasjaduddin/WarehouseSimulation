d22f3db04d23b532fcf3797c8665e23cc4cbe965
kasjaduddin <kasjaduddin@outlook.com>
2024-12-14 22:14:06 +0700

    feat: update reset input field on bin registration manager

--- Assets/Scripts/BinRegistrationManager.cs
+++ Assets/Scripts/BinRegistrationManager.cs
@@ -1,29 +1,34 @@
 using UnityEngine;
 using TMPro;
 
-public class BinRegistrationManager : MonoBehaviour
+namespace CompanySystem
 {
-    public GoogleFormConnector connector; // Reference to the Google Form connector
-    public TMP_InputField codeInputField;
-    public TMP_InputField informationInputField;
-    private string code; // Variable to store the bin code
-    private string information; // Variable to store the additional information
-
-    // Register new bin to system
-    public void AddNewBin()
+    public class BinRegistrationManager : MonoBehaviour
     {
-        code = codeInputField.text;
-        information = informationInputField.text;
+        public GoogleFormConnector connector; // Reference to the Google Form connector
+        public TMP_InputField codeInputField;
+        public TMP_InputField informationInputField;
+        private string code; // Variable to store the bin code
+        private string information; // Variable to store the additional information
 
-        // Write data to the Google Form
-        StartCoroutine(connector.WriteData(code, information));
-        ResetInput();
-    }
+        // Register new bin to system
+        public void AddNewBin()
+        {
+            code = codeInputField.text;
+            information = informationInputField.text;
 
-    // Reset input field
-    public void ResetInput()
-    {
-        codeInputField.text = codeInputField.text.Remove(0);
-        informationInputField.text = informationInputField.text.Remove(0);
+            // Write data to the Google Form
+            StartCoroutine(connector.WriteData(code, information));
+            ResetInput();
+        }
+
+        // Reset input field
+        public void ResetInput()
+        {
+            if (codeInputField.text.Length > 0)
+                codeInputField.text = codeInputField.text.Remove(0);
+            if (informationInputField.text.Length > 0)
+                informationInputField.text = informationInputField.text.Remove(0);
+        }
     }
 }
