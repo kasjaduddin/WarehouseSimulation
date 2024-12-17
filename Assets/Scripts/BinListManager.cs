23fe30c0d97a0fbf44285683103cda5f19d793fb
kasjaduddin <kasjaduddin@outlook.com>
2024-12-17 21:31:11 +0700

    feat: update bin list table

--- Assets/Scripts/BinListManager.cs
+++ Assets/Scripts/BinListManager.cs
@@ -2,79 +2,91 @@ using UnityEngine;
 using UnityEngine.UI;
 using Newtonsoft.Json.Linq;
 using TMPro;
+using System.Collections.Generic;
 
-public class BinListManager : MonoBehaviour
+
+namespace CompanySystem
 {
-    public GoogleSheetsConnector connector; // Reference to the Google Sheets connector
-    private JArray bins; // Array to store bins data
-    public GameObject recordTemplate; // Template for displaying each record
-    public Transform container; // Container to hold the instantiated records
-
-    // Start is called before the first frame update
-    void OnEnable()
+    public class BinListManager : MonoBehaviour
     {
-        // Invoke GetData method after a short delay
-        bins = JArray.Parse(@"[[null, '', '']]");
-        Invoke("GetData", 0.1f);
-    }
+        //public GoogleSheetsConnector connector; // Reference to the Google Sheets connector
+        private JArray bins; // Array to store bins data
+        public GameObject recordTemplate; // Template for displaying each record
+        public Transform container; // Container to hold the instantiated records
+        public static Record selectedRecord;
 
-    // Get all bin data from Google Sheets
-    public void GetData()
-    {
-        StartCoroutine(connector.ReadData("Bins", (data) =>
+        // Start is called before the first frame update
+        void OnEnable()
         {
-            // Parse JSON response
-            JObject parsedData = JObject.Parse(data);
-            bins = (JArray)parsedData["values"];
-            bins.RemoveAt(0);
-            ShowRecord();
-        }));
-    }
-
-    // Display all bin data to bin table
-    void ShowRecord()
-    {
-        float templateHigh = 91f;
-        for (int i = 0; i < bins.Count; i++) 
-        {
-            GameObject newRow = Instantiate(recordTemplate, container);
-            Transform newRowTransform = newRow.transform;
-            RectTransform entryRectTransform = newRow.GetComponent<RectTransform>();
-
-            // Fill the UI elements with data
-            entryRectTransform.anchoredPosition = new Vector2(0f, 228f + (-templateHigh * i));
-            newRowTransform.Find("Id").GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
-            newRowTransform.Find("Code").GetComponent<TextMeshProUGUI>().text = bins[i][1].ToString();
-            newRowTransform.Find("Information").GetComponent<TextMeshProUGUI>().text = bins[i][2].ToString();
+            // Invoke GetData method after a short delay
+            Invoke("GetData", 0.1f);
         }
-        recordTemplate.SetActive(false);
-    }
 
-    private void GetRecordData(Transform recordTransform)
-    {
-        string id = recordTransform.Find("Id").GetComponent<TextMeshProUGUI>().text;
-        string code = recordTransform.Find("Code").GetComponent<TextMeshProUGUI>().text;
-        string information = recordTransform.Find("Information").GetComponent<TextMeshProUGUI>().text;
-
-        // Output the data to the console
-        Debug.Log($"Id: {id}, Code: {code}, Information: {information}");
-    }
-    // Edit selected record
-    public void EditRecord(Transform recordTransform)
-    {
-        GetRecordData(recordTransform);
-    }
-
-    // Delete shows all bin data in the bin table
-    public void DestroyRecord()
-    {
-        foreach (Transform child in container)
+        // Get all bin data from Google Sheets
+        public void GetData()
         {
-            if (child != recordTemplate.transform)
+            StartCoroutine(FirebaseServices.ReadData("bins", data =>
             {
-                Destroy(child.gameObject);
-            }
+                if (data != null)
+                {
+                    bins = data;
+                    ShowRecord();
+                }
+                else
+                {
+                    Debug.LogError("Failed to retrieve data.");
+                }
+            }));
         }
-        recordTemplate.SetActive(true);
+
+        // Display all bin data to bin table
+        void ShowRecord()
+        {
+            float templateHigh = 91f;
+            foreach (var bin in bins)
+            {
+                GameObject newRow = Instantiate(recordTemplate, container);
+                Transform newRowTransform = newRow.transform;
+                RectTransform entryRectTransform = newRow.GetComponent<RectTransform>();
+
+                // Fill the UI elements with data
+                entryRectTransform.anchoredPosition = new Vector2(0f, 228f + (-templateHigh * ((int)bin["id"] - 1)));
+                newRowTransform.Find("Id").GetComponent<TextMeshProUGUI>().text = bin["id"].ToString();
+                newRowTransform.Find("Code").GetComponent<TextMeshProUGUI>().text = bin["code"].ToString();
+                newRowTransform.Find("Information").GetComponent<TextMeshProUGUI>().text = bin["information"].ToString();
+                newRowTransform.Find("Number of Tag").GetComponent<TextMeshProUGUI>().text = bin["numberoftags"].ToString();
+            }
+            recordTemplate.SetActive(false);
+        }
+
+        private Record GetRecord(Transform recordTransform)
+        {
+            string id = recordTransform.Find("Id").GetComponent<TextMeshProUGUI>().text;
+            string code = recordTransform.Find("Code").GetComponent<TextMeshProUGUI>().text;
+            string information = recordTransform.Find("Information").GetComponent<TextMeshProUGUI>().text;
+
+            // Create a BinRecord struct and return it
+            Record record = new Record(id, code, information);
+            return record;
+        }
+
+        // Edit selected record
+        public void EditRecord(Transform recordTransform)
+        {
+            selectedRecord = GetRecord(recordTransform);
+        }
+
+        // Delete shows all bin data in the bin table
+        public void DestroyRecord()
+        {
+            foreach (Transform child in container)
+            {
+                if (child != recordTemplate.transform)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+            recordTemplate.SetActive(true);
+        }
     }
 }
