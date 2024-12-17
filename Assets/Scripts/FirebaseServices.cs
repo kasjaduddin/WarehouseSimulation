14922fde4b55ca1c6f944d258886ee6111f3244b
kasjaduddin <kasjaduddin@outlook.com>
2024-12-17 14:03:21 +0700

    feat: add write data to real-time database firebase

--- Assets/Scripts/FirebaseServices.cs
+++ Assets/Scripts/FirebaseServices.cs
@@ -7,7 +7,7 @@ using Firebase.Extensions;
 
 public class FirebaseServices : MonoBehaviour
 {
-    private DatabaseReference reference;
+    private static DatabaseReference reference;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +26,45 @@ public class FirebaseServices : MonoBehaviour
             else
             {
                 Debug.LogError("Could not resolve all Firebase dependencies: " + task.Result);
+            }
+        });
+    }
+
+    public static void WriteData(string collectionName, Dictionary<string, object> data)
+    {
+
+        reference.Child(collectionName).OrderByKey().LimitToLast(1).GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                int newId = 1; // Default to 1 if no data exists
+                
+                // Find the next available ID
+                foreach (DataSnapshot child in snapshot.Children)
+                {
+                    int lastId = int.Parse(child.Key);
+                    newId = lastId + 1;
+                }
+
+                string documentId = newId.ToString();
+                DatabaseReference docRef = reference.Child(collectionName).Child(documentId);
+
+                docRef.SetValueAsync(data).ContinueWithOnMainThread(innerTask =>
+                {
+                    if (innerTask.IsCompleted)
+                    {
+                        Debug.Log($"{documentId} added to {collectionName} collection.");
+                    }
+                    else
+                    {
+                        Debug.LogError($"Failed to add {documentId} to {collectionName} collection: {innerTask.Exception}");
+                    }
+                });
+            }
+            else
+            {
+                Debug.LogError($"Failed to find last ID in {collectionName} collection: {task.Exception}");
             }
         });
     }
