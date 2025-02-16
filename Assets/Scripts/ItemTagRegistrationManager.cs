f93cb4d3b0513bb802be2e1d75dd9f190d34a9ef
kasjaduddin <kasjaduddin@outlook.com>
2025-02-16 18:03:04 +0700

    feat: add item tag registration manager

---
+++ Assets/Scripts/ItemTagRegistrationManager.cs
@@ -0,0 +1,164 @@
+using CompanySystem;
+using Newtonsoft.Json.Linq;
+using Record;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering.LookDev;
+using UnityEngine.UI;
+using static Record.TransactionRecord;
+
+namespace Rfid
+{
+    public class ItemTagRegistrationManager : MonoBehaviour
+    {
+        private TMP_Dropdown transactionCodeDropdown;
+
+        private GameObject table;
+        private Transform container; // Container to hold the instantiated records
+        private GameObject recordTemplate; // Template for displaying each record
+        private TransactionRecord transactionRecord;
+
+        private void OnEnable()
+        {
+            transactionCodeDropdown = transform.Find("Transaction Code Dropdown").GetComponent<TMP_Dropdown>();
+            table = transform.Find("Item List").gameObject;
+            container = table.transform.Find("Table Container");
+            recordTemplate = container.Find("Record Template").gameObject;
+            Invoke("GetTransactionCodes", 0.1f);
+        }
+
+        private void OnDisable()
+        {
+            transactionCodeDropdown.options.Clear();
+            DestroyRecord();
+        }
+
+        public void GetTransactionCodes()
+        {
+            StartCoroutine(FirebaseServices.ReadData("transactions", data =>
+            {
+                if (data != null)
+                {
+                    foreach (var item in data)
+                    {
+                        transactionCodeDropdown.options.Add(new TMP_Dropdown.OptionData(item["code"].ToString()));
+                    }
+                    transactionCodeDropdown.captionText.text = transactionCodeDropdown.options[0].text;
+                    Invoke("LoadTransactionData", 0.1f);
+                }
+                else
+                {
+                    Debug.LogError("Failed to retrieve data.");
+                }
+            }));
+        }
+
+        public void LoadTransactionData()
+        {
+            DestroyRecord();
+            StartCoroutine(FirebaseServices.ReadData("transactions", "code", transactionCodeDropdown.captionText.text, data =>
+            {
+                if (data != null)
+                {
+                    TransactionRecord record = new TransactionRecord(data);
+                    transactionRecord = record;
+                    ShowItems();
+                }
+                else
+                {
+                    Debug.LogError("Failed to retrieve data.");
+                }
+            }));
+        }
+
+        public void SelectItem(TextMeshProUGUI sku)
+        {
+            ResetInformation();
+
+            Transform button = sku.transform.parent;
+            button.GetComponent<Image>().color = new Color32(4, 83, 221, 255);
+
+            StartCoroutine(FirebaseServices.ReadData("transactions", "code", transactionCodeDropdown.captionText.text, "items", "sku", sku.text, data =>
+            {
+                if (data != null)
+                {
+                    TransactionItem record = new TransactionItem(data);
+                    ShowItemDetail(sku.text, record.Quantity.ToString());
+                }
+                else
+                {
+                    Debug.LogError("Failed to retrieve data.");
+                }
+            }));
+        }
+
+        private void ShowItems()
+        {
+            float templateHigh = 32f;
+            for (int i = 0; i < transactionRecord.Items.Count; i++)
+            {
+                TransactionItem item = transactionRecord.Items[i];
+
+                GameObject newRow = Instantiate(recordTemplate, container);
+                Transform newRowTransform = newRow.transform;
+                RectTransform entryRectTransform = newRow.GetComponent<RectTransform>();
+
+                // Fill the UI elements with data
+                entryRectTransform.anchoredPosition = new Vector2(0f, 46f + (-templateHigh * i));
+                newRowTransform.Find("Button").Find("SKU").GetComponent<TextMeshProUGUI>().text = item.Sku;
+
+                table.GetComponent<DynamicTableManager>().enabled = true;
+            }
+            recordTemplate.SetActive(false);
+        }
+
+        private void ShowItemDetail(string sku, string quantity)
+        {
+            StartCoroutine(FirebaseServices.ReadData("items", "sku", sku, data =>
+            {
+                if (data != null)
+                {
+                    Transform itemInformation = transform.Find("Item Information");
+                    itemInformation.Find("SKU").GetComponent<TextMeshProUGUI>().text = data["sku"].ToString();
+                    itemInformation.Find("Item Name").GetComponent<TextMeshProUGUI>().text = data["item_name"].ToString();
+                    itemInformation.Find("Bin Code").GetComponent<TextMeshProUGUI>().text = data["bin_code"].ToString();
+                    itemInformation.Find("Stock").GetComponent<TextMeshProUGUI>().text = data["quantity"].ToString();
+                    itemInformation.Find("Quantity").GetComponent<TextMeshProUGUI>().text = quantity;
+                    itemInformation.Find("Tags").GetComponent<TextMeshProUGUI>().text = data["number_of_tags"].ToString();
+                }
+                else
+                {
+                    Debug.LogError("Failed to retrieve data.");
+                }
+            }));
+        }
+
+        // Delete shows all item data in the item table
+        private void DestroyRecord()
+        {
+            ResetInformation();
+            foreach (Transform child in container)
+            {
+                if (child != recordTemplate.transform)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+            recordTemplate.SetActive(true);
+        }
+
+        private void ResetInformation()
+        {
+            foreach (Transform child in container)
+            {
+                child.Find("Button").GetComponent<Image>().color = new Color32(255, 255, 255, 0);
+            }
+
+            foreach (Transform child in transform.Find("Item Information"))
+            {
+                child.GetComponent<TextMeshProUGUI>().text = "";
+            }
+        }
+    }
+}
